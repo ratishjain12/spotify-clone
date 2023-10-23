bd9b7644437da51eb32ecdf498addd126e511bc1
ratishjain <ratishjain6@gmail.com>
2023-10-23 15:08:02 +0530

    sidebard done

---
+++ components/Button.tsx
@@ -0,0 +1,6 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = () => {
+  return <div>Button</div>;
+};
+export default Button;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 interface SidebarProps {
   children: React.ReactNode;
 }
@@ -38,8 +39,11 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             })}
           </div>
         </Box>
-        <Box className=" h-full">Song Library</Box>
+        <Box className=" h-full">
+          <Library />
+        </Box>
       </div>
+      <main className=" h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
